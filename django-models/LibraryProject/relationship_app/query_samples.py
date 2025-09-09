c671c3077fc583cc8de4851e4a6214ba4e8d9cb4
kimanithuo1 <jkimanithuo@gmail.com>
2025-09-09 11:17:26 +0300

    added a new relationship app in django models dir

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,7 +2,8 @@ from .models import Author, Book, Library
 
 def queries():
     # 1. Query all books by a specific author
-    author = Author.objects.get(name="George Orwell")
+    author_name = "George Orwell"
+    author = Author.objects.get(name=author_name)
     books_by_author = Book.objects.filter(author=author)
 
     # 2. List all books in a library
@@ -11,7 +12,7 @@ def queries():
     books_in_library = library.books.all()
 
     # 3. Retrieve the librarian for a library
-    librarian = library.librarian  
+    librarian = library.librarian
 
     return books_by_author, books_in_library, librarian
 # Note: The above code assumes that the database has been populated with relevant data.
