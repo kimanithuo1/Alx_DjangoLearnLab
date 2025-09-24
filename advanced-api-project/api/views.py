84b6e494285cd8f960fcfa140d650b7320e1ec8c
kimanithuo1 <jkimanithuo@gmail.com>
2025-09-24 14:43:49 +0300

    added generic views in api app

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+
+# List all books / Create a new book
+class BookListCreateView(generics.ListCreateAPIView):
+    """
+    GET: Retrieve all books
+    POST: Create a new book (requires authentication)
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    # Allow unauthenticated users to view, but restrict creation
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+# Retrieve a single book / Update / Delete
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    GET: Retrieve book by ID
+    PUT/PATCH: Update book (requires authentication)
+    DELETE: Delete book (requires authentication)
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    # Allow unauthenticated users to read, but restrict modification
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
